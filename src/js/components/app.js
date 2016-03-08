e5b03fe3157847386c17e4d0afb418ca0eb8fad4
sohilpandya <sohilpandya@me.com>
2016-03-08 17:50:00 +0000

    setup completed, added react, hot-loader and router basics with the /public/ endpoint returning hello world! next step to set up css. #1

---
+++ src/js/components/app.js
@@ -0,0 +1,13 @@
+var React = require('react');
+
+var App = React.createClass({
+  render: function() {
+    return (
+    <div className="landing-page">
+      Hello World!!
+    </div>
+    )
+  }
+});
+
+module.exports = App;
