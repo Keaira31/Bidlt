2ab46770e47a15092e0096c64c88a370d1b8e9b6
Katerina Pascoulis <katerina.pascoulis@gmail.com>
2016-03-11 15:06:36 +0000

    trying to view adgenda items #9

--- src/js/components/global/agenda.js
+++ src/js/components/global/agenda.js
@@ -1,6 +1,7 @@
 var React = require('react');
 var BigCalendar = require('react-big-calendar');
 var moment = require('moment');
+import events from '../builder/event.js'
 require('react-big-calendar/lib/css/react-big-calendar.css');
 
 BigCalendar.momentLocalizer(moment);
@@ -9,25 +10,14 @@ var Agenda = React.createClass({
   render: function(){
 
     return (
+
       <div className="agenda-overview main-padding margin-30">
         <h5>Agenda</h5>
         <BigCalendar
-          events={[{
-            "title" : "All Day Event",
-            "allDay": true,
-            "start" : new Date(2016, 3, 11),
-            "end" : new Date(2016, 3, 11)
-          },
-          {
-            "title" : "Sohil Day",
-            "start" : new Date(2016, 2, 12, 12),
-            "end" : new Date(2016, 2, 12, 13)
-          }
-        ]}
+          events={events}
           views={['agenda']}
-          view={'agenda'}
-          startAccessor='startDate'
-          endAccessor='endDate'
+          defaultView='agenda'
+          defaultDate={new Date(2015, 3, 1)}
         />
       </div>
     );
