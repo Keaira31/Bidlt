f68dcf1b5d635372fb41cf77ba0feed91b123c28
Katerina Pascoulis <katerina.pascoulis@gmail.com>
2016-03-11 17:33:59 +0000

    structured child routing #62

---
+++ src/js/components/builder/builder-template.js
@@ -0,0 +1,25 @@
+var React = require('react');
+var SideNav = require('../global/side-nav.js');
+var Header = require('../global/header.js');
+var Footer = require('../landing-page/landing-footer.js');
+
+
+var BuilderTemplate = React.createClass({
+
+  render: function(){
+    var navArr = [{name:'Overview',url:'/BuilderTemplate', icon:'dashboard'}, {name:'Current Jobs',url:'/BuilderTemplate/CurrentJobs', icon:'event'}, {name:'Available Jobs',url:'/BuilderTemplate/AvailableJobs', icon: 'work'},{name:'Back Office',url:'/backOffice', icon: 'receipt'},{name:'Wall',url:'/wall', icon:'chat_bubble_outline'},{name:'Search & Hire',url:'/seachHire', icon: 'build'}, {name:'Safeguarding',url:'/safeguarding', icon: 'thumb_up'}];
+
+
+    return (
+      <div className="">
+        <SideNav navArray={navArr} interface={'BILDT: Builder'}/>
+        <Header name={"Sohil's Company"} address={'14 Palmers Road, Mile End, London'}/>
+        {this.props.children}
+        <Footer />
+
+      </div>
+    );
+  }
+});
+
+module.exports = BuilderTemplate;
--- src/js/index.js
+++ src/js/index.js
@@ -12,7 +12,11 @@ require('../css/index.js');
 
 
 var App = require('./components/app.js');
+var BuilderTemplate = require('./components/builder/builder-template.js');
 var BuilderOverview = require('./components/builder/builder-overview.js');
+var BuilderCurrentJobs = require('./components/builder/current-jobs.js');
+var AvailableJobs = require('./components/builder/available-jobs.js');
+
 var CustomerOverview = require('./components/customer/customer-overview.js');
 var SbspOverview = require('./components/sbsp/sbsp-overview.js');
 
@@ -22,7 +26,12 @@ var rootElement = document.getElementById('react_content_container');
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path='/' component={App} />
-    <Route path='/BuilderOverview' component={BuilderOverview} />
+    <Route path='/BuilderTemplate' component={BuilderTemplate}>
+      <IndexRoute path='BuilderOverview' component={BuilderOverview}/>
+      <Route path='CurrentJobs' component={BuilderCurrentJobs}/>
+      <Route path='AvailableJobs' component={AvailableJobs}/>
+    </Route>
+
     <Route path='/CustomerOverview' component={CustomerOverview} />
     <Route path='/SbspOverview' component={SbspOverview} />
 
