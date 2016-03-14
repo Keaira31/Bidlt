4676f70f479590be353448023bb01eafa55cadb6
Katerina Pascoulis <katerina.pascoulis@gmail.com>
2016-03-14 17:31:50 +0000

    #19 Added Job Results info

--- src/js/components/builder/available-jobs.js
+++ src/js/components/builder/available-jobs.js
@@ -1,11 +1,13 @@
 var React = require('react');
 var JobSearch = require('./job-search.js');
+var JobSearchResult = require('./job-search-result.js');
 
 var AvailableJobs = React.createClass({
   render: function(){
     return (
       <div className="builder-available-jobs">
         <JobSearch />
+        <JobSearchResult />
       </div>
     );
   }
