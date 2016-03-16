043c629248d1bf03a87b278069de39c507aff749
Katerina Pascoulis <katerina.pascoulis@gmail.com>
2016-03-16 16:40:56 +0000

    Added back button to avalilable jobs page, fixed alignment #21 #22

--- src/js/components/builder/job-tender.js
+++ src/js/components/builder/job-tender.js
@@ -3,6 +3,7 @@ var JobData = require('./job-data.js');
 var Gallery = require('react-image-gallery');
 var TenderGoogleMap = require('./tender-google-map.js');
 var TenderModal = require('./job-tender-modal.js');
+var Link = require('react-router').Link;
 require('react-image-gallery/build/image-gallery.css');
 
 
@@ -40,10 +41,17 @@ var JobTender = React.createClass({
   render: function(){
     console.log("this is the images", this.state.job.images);
     return (
-
       <div className="job-tender-component main-padding margin-30">
-        <div className="component-title-box">
-          <h5 className="center-align"> {this.state.job.title} </h5>
+
+        <div className="row">
+          <div className="back-button-container col l3 s12 left-align">
+            <Link to="/BuilderTemplate/AvailableJobs" className="btn-flat waves-effect waves-light darkest-text-color">
+            <i className="material-icons left darkest-text-color">keyboard_backspace</i> Back</Link>
+          </div>
+
+          <div className="center-align col l6 s12">
+            <h4 className="center-align"> {this.state.job.title} </h4>
+          </div>
         </div>
 
         <div className="row">
@@ -54,9 +62,18 @@ var JobTender = React.createClass({
 
         <div className="row">
           <div className="job-text col s12 l6">
-            <h4> Job Details</h4>
+            <h5> Job Details</h5>
             <p> {this.state.job.jobDescription}</p>
+
+            <h5> Files Uploaded</h5>
+              <ul className="collection with-header">
+                <li className="collection-item"><div>File 1<a href="http://www.planningportal.gov.uk/uploads/br/BR_PDF_AD_A_2013.pdf" download="business-regs" className="secondary-content"><i className="material-icons darkest-text-color">file_download</i></a></div></li>
+                <li className="collection-item"><div>File 2<a href="http://www.planningportal.gov.uk/uploads/br/BR_PDF_AD_A_2013.pdf" download="business-regs" className="secondary-content"><i className="material-icons darkest-text-color">file_download</i></a></div></li>
+                <li className="collection-item"><div>File 3<a href="http://www.planningportal.gov.uk/uploads/br/BR_PDF_AD_A_2013.pdf" download="business-regs" className="secondary-content"><i className="material-icons darkest-text-color">file_download</i></a></div></li>
+                <li className="collection-item"><div>File 4<a href="http://www.planningportal.gov.uk/uploads/br/BR_PDF_AD_A_2013.pdf" download="business-regs" className="secondary-content"><i className="material-icons darkest-text-color">file_download</i></a></div></li>
+              </ul>
           </div>
+
 
           <div className="col s12 l6">
             <TenderGoogleMap />
