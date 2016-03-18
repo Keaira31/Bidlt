f0271775814b2a2c2ded5ba43430c78a1adf0adc
Katerina Pascoulis <katerina.pascoulis@gmail.com>
2016-03-18 17:37:30 +0000

    changed icons on customer page

--- src/js/components/customer/advice.js
+++ src/js/components/customer/advice.js
@@ -1,13 +1,15 @@
 var React = require('react');
+var ComingSoon = require('../global/coming-soon.js');
 
-var Inspiration = React.createClass({
+var Advice = React.createClass({
   render: function(){
     return (
       <div className="">
+        <ComingSoon pageName={"Sohil's Company"} icon={}/>
         <h1>Inspiration</h1>
       </div>
     );
   }
 });
 
-module.exports = Inspiration;
+module.exports = Advice;
--- src/js/components/customer/customer-template.js
+++ src/js/components/customer/customer-template.js
@@ -7,12 +7,12 @@ var Footer = require('../global/footer.js');
 var CustomerTemplate = React.createClass({
 
   render: function(){
-    var navArr = [{name:'Current Work',url:'/CustomerTemplate', icon:'dashboard'}, {name:'Tasks',url:'/CustomerTemplate/CustomerTasks', icon:'event'}, {name:'Request Work',url:'/CustomerTemplate/RequestWork', icon: 'work'},{name:'Inspiration',url:'/CustomerTemplate/Inspiration', icon: 'receipt'},{name:'Advice',url:'/CustomerTemplate/Advice', icon:'chat_bubble_outline'},{name:'Job Security',url:'/CustomerTemplate/JobSecurity', icon: 'build'}];
+    var navArr = [{name:'Current Work',url:'/CustomerTemplate', icon:'event'}, {name:'Tasks',url:'/CustomerTemplate/CustomerTasks', icon:'list'}, {name:'Request Work',url:'/CustomerTemplate/RequestWork', icon: 'open_in_browser'},{name:'Inspiration',url:'/CustomerTemplate/Inspiration', icon: 'star_border'},{name:'Advice',url:'/CustomerTemplate/Advice', icon:'people_outline'},{name:'Job Security',url:'/CustomerTemplate/JobSecurity', icon: 'vpn_key'}];
     console.log("rendering customer template");
 
     return (
       <div className="">
-        <SideNav navArray={navArr} interface={'BILDT: Builder'}/>
+        <SideNav navArray={navArr} interface={'BILDT: Customer'}/>
         <Header name={"Sarah's House Build"} address={'23 Bricklane, Aldgate, London'}/>
         {this.props.children}
         <Footer />
