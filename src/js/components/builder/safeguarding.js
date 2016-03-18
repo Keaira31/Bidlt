e226e35c7908391c49ef952de7548b114b83702e
Katerina Pascoulis <katerina.pascoulis@gmail.com>
2016-03-18 15:12:03 +0000

    created saefguarding placeholder and styling #29

--- src/js/components/builder/safeguarding.js
+++ src/js/components/builder/safeguarding.js
@@ -3,8 +3,17 @@ var React = require('react');
 var Safeguarding = React.createClass({
   render: function(){
     return (
-      <div className="">
+      <div className="row">
+      <div className="safeguarding-outer push-footer-to-bottom main-padding margin-30">
+        <div className="col s12 l12 z-depth-1">
+        <h3 className="center-align">Safeguarding</h3>
+          <div className="center-align">
+            <i className="material-icons large darkest-text-color">lock_outline</i>
+          </div>
+        <h5 className="center-align">Coming Soon</h5>
+        </div>
 
+      </div>
       </div>
     );
   }
