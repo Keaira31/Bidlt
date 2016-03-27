ca6b56f53266d2081fd5163ebcdc143e5831094c
sohilpandya <sohilpandya@me.com>
2016-03-27 11:05:08 +0100

    @katpas I have changed some css for the footer and the about us text as on a high resolution screen it was off center, it should now be centered on all screens. Pull it down check it out before we can merge. also I Love the new changes

--- src/js/components/landing-page/landing-footer.js
+++ src/js/components/landing-page/landing-footer.js
@@ -7,12 +7,12 @@ var LandingFooter = React.createClass({
 
       <footer className="page-footer">
           <div className="container">
-            <div className="row">
+            <div className="row about-us-footer-main">
               <div className="col l6 s12">
                 <h5 className="white-text">The BILDT Platform</h5>
                 <p className="">You can access our different platforms via our sign in button or these links</p>
               </div>
-              <div className="col l4 offset-l2 s12">
+              <div className="col l4 offset-l2 s12 about-us-footer-sign-in">
                 <h5 className="white-text">I am a...</h5>
                 <ul>
                   <li><Link to="/BuilderTemplate" className="" href="#!">Builder</Link></li>
